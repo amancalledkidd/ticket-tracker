771219bdfd8a8d56cc1ca4b2d6522e47520dd3fc
Kumani K <kumanikidd@gmail.com>
2023-12-12 12:49:53 +0000

    Functionality for handle increment and decrement complete

--- src/components/TicketCounter/TicketCounter.tsx
+++ src/components/TicketCounter/TicketCounter.tsx
@@ -1,20 +1,27 @@
 import { useState } from "react"
+import minus from '../../assets/minus.svg'
+import plus from '../../assets/plus.svg'
 
 const TicketCounter = () => {
 
     const [ticketCount, setTicketCount] = useState<number>(0)
 
-    const increment = () => {
+    const handleIncrement = () => {
         setTicketCount(ticketCount + 1)
     }
 
-    const decrement = () => {
-        setTicketCount(ticketCount - 1)
+    const handleDecrement = () => {
+        if (ticketCount > 0) {
+            setTicketCount(ticketCount - 1)
+        }
     }
 
     return (
         <div>
-            <h1>Counter</h1>
+            <h1>Tickets</h1>
+            <img src={minus} alt="minus" onClick={handleDecrement}/>
+            <p>{ticketCount}</p>
+            <img src={plus} alt="minus" onClick={handleIncrement}/>
         </div>
     )
 }
